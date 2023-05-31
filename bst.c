802454e8a99bd8aac47687d0c815b11702d5efa5
Leite | Vinicius Leite Costa <92338016+ViniciusLeiteCosta@users.noreply.github.com>
2023-05-31 16:20:55 -0300

    Remover Implementado

--- bst.c
+++ bst.c
@@ -31,4 +31,48 @@ arvore preorder_bst(arvore raiz){
         preorder_bst(raiz->esq);
         preorder_bst(raiz->dir);
     }
-}    
+}
+
+//REMOVER
+arvore remover_bst(arvore raiz, int valor){
+    
+    //Remover de uma subárvore vaiza / remover elemento inexistente
+    if (raiz == NULL){
+        return raiz;
+    }
+    
+    if (raiz->valor == valor){
+        
+        //CASO 1: NÃO TEM FILHOS.
+        if(raiz->esq == NULL && raiz->dir == NULL){
+            free(raiz);
+            return NULL;
+        }
+        
+        //CASO 2: TEM UM FILHO ESQUERDO.
+        if(raiz->esq != NULL && raiz->dir == NULL){
+            arvore aux = raiz->esq;
+            free(raiz);
+            return aux;
+        }
+        
+        //CASO 3: QUANDO O NÓ TEM DOIS FILHOS.
+        //NÃO PRECISA DE CONDICIONAL 
+       
+        int maiorValorSubarvoreEsquerda = maior(raiz->esq)->valor;
+        raiz->valor = maiorValorSubarvoreEsquerda;
+        raiz->esq = remover_bst(raiz->esq, maiorValorSubarvoreEsquerda);
+        
+        return raiz;
+    }
+    
+    /*Se o valor a ser removido for maior que o valor da raiz atual, chame recursivamente a função remover_bst na subárvore direita.
+    Caso contrário, chame recursivamente a função remover_bst na subárvore esquerda.
+    Retorne a raiz atual após a remoção ter sido feita em uma das subárvores (ou sem alterações se o valor não for encontrado).*/
+    if(valor > raiz->valor) {
+       raiz->dir = remover_bst(raiz->dir, valor);
+    } else {
+        //É simétrico
+    }
+    return raiz;
+}
