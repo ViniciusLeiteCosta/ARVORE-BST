ec8024b521631ace33d7c19196702559a4559aeb
Leite | Vinicius Leite Costa <92338016+ViniciusLeiteCosta@users.noreply.github.com>
2023-06-01 19:21:45 -0300

    Erro do primo corrigido.

--- bst.c
+++ bst.c
@@ -90,21 +90,6 @@ void reverso_bst(arvore raiz) {
     }
 }
 
-//VERIFICAÇÃO DE NÚMERO PRIMO
-int Primo(int n) {
-    if (n <= 1) {
-        return 0;
-    }
-
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return 0;
-        }
-    }
-
-    return 1;
-}
-
 //QUANTIDADE DE PRIMOS NA ÁRVORE
 int qtdPrimo(arvore raiz) {
     if (raiz == NULL) {
